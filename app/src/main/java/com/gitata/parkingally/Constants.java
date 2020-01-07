4110e5a7b1ae7911bef8a69a340ac0f6371b5318
Mathew Gitata <gitataIT@gmail.com>
2020-01-07 20:23:11 +0300

    Add api credentials

---
+++ app/src/main/java/com/gitata/parkingally/Constants.java
@@ -0,0 +1,6 @@
+package com.gitata.parkingally;
+
+public class Constants {
+    public static final String EMEL_BASE_URL = "https://emel.city-platform.com/opendata/parking/lots";
+    public static final String EMEL_API_KEY = BuildConfig.EMEL_API_KEY;
+}
