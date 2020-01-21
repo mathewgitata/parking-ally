139166e3bc352f5ae1aeb51d4f405c007c6c11dd
Mathew Gitata <mathewgitata@gmail.com>
2020-01-21 05:47:09 +0300

    Add navigation drawer and its components

--- app/src/main/java/com/gitata/parkingally/ui/ParkingLotListActivity.java
+++ app/src/main/java/com/gitata/parkingally/ui/ParkingLotListActivity.java
@@ -1,9 +1,5 @@
 package com.gitata.parkingally.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 
 import com.gitata.parkingally.R;
@@ -11,6 +7,28 @@ import com.gitata.parkingally.adapters.ParkingLotListAdapter;
 import com.gitata.parkingally.models.EmelParkingLotResponse;
 import com.gitata.parkingally.network.EmelParkingApi;
 import com.gitata.parkingally.network.EmelParkingClient;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import android.view.View;
+
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.Menu;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +40,7 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class ParkingLotListActivity extends AppCompatActivity {
+
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
     private ParkingLotListAdapter mAdapter;
@@ -33,6 +52,12 @@ public class ParkingLotListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_parking_lot_list);
         ButterKnife.bind(this);
 
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
         EmelParkingApi client = EmelParkingClient.getClient();
         Call<List<EmelParkingLotResponse>> call = client.getParkingLots();
 
@@ -41,10 +66,10 @@ public class ParkingLotListActivity extends AppCompatActivity {
             public void onResponse(Call<List<EmelParkingLotResponse>> call, Response<List<EmelParkingLotResponse>> response) {
                 if (response.isSuccessful()) {
                     mParkingLotsList.addAll(response.body());
-                    mAdapter = new ParkingLotListAdapter(ParkingLotListActivity.this, mParkingLotsList);
+                    mAdapter = new ParkingLotListAdapter(com.gitata.parkingally.ui.ParkingLotListActivity.this, mParkingLotsList);
 
                     RecyclerView.LayoutManager layoutManager =
-                            new LinearLayoutManager(ParkingLotListActivity.this);
+                            new LinearLayoutManager(com.gitata.parkingally.ui.ParkingLotListActivity.this);
                     mRecyclerView.setLayoutManager(layoutManager);
                     mRecyclerView.setHasFixedSize(true);
 
@@ -56,8 +81,15 @@ public class ParkingLotListActivity extends AppCompatActivity {
             public void onFailure(Call<List<EmelParkingLotResponse>> call, Throwable t) {
             }
         });
-
     }
 
-
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
