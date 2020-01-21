1668c4ffc6058aa74303e0a2bcc5dba0ae1512c6
Mathew Gitata <mathewgitata@gmail.com>
2020-01-21 11:44:10 +0300

    Setup onclick handlers for the various nav bar items

--- app/src/main/java/com/gitata/parkingally/ui/ParkingLotListActivity.java
+++ app/src/main/java/com/gitata/parkingally/ui/ParkingLotListActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.gitata.parkingally.R;
 import com.gitata.parkingally.adapters.ParkingLotListAdapter;
 import com.gitata.parkingally.models.EmelParkingLotResponse;
+import com.gitata.parkingally.models.NavBarItem;
 import com.gitata.parkingally.network.EmelParkingApi;
 import com.gitata.parkingally.network.EmelParkingClient;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -29,6 +30,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.Menu;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +42,8 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ParkingLotListActivity extends AppCompatActivity {
+public class ParkingLotListActivity extends AppCompatActivity implements View.OnClickListener {
+    private DrawerLayout drawer;
 
     @BindView(R.id.recycler_view)
     RecyclerView mRecyclerView;
@@ -83,6 +87,14 @@ public class ParkingLotListActivity extends AppCompatActivity {
         });
     }
 
+    private void setNavBarButtons() {
+        for (NavBarItem item : NavBarItem.values()) {
+            TextView itemView = findViewById(item.getItemId());
+            itemView.setOnClickListener(this);
+        }
+    }
+
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -91,5 +103,56 @@ public class ParkingLotListActivity extends AppCompatActivity {
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        drawer = findViewById(R.id.drawer_layout);
+        switch (NavBarItem.fromViewId(v.getId())) {
+            case MY_ACCOUNT:
+                drawer.closeDrawer(GravityCompat.START);
+                toAccountSettings();
+                break;
+            case PARKING_HISTORY:
+                drawer.closeDrawer(GravityCompat.START);
+                toParkingHistory();
+                break;
+            case PAYMENT:
+                drawer.closeDrawer(GravityCompat.START);
+                toPaymentInfo();
+                break;
+            case HELP:
+                drawer.closeDrawer(GravityCompat.START);
+                toFeedback();
+                break;
+            case LEGAL:
+                drawer.closeDrawer(GravityCompat.START);
+                toLegalDetails();
+                break;
+            default:
+                Toast.makeText(this, "error", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+    }
+
+    private void toLegalDetails() {
+        //TODO: Redirect user to legal page
+    }
+
+    private void toFeedback() {
+        //TODO: Redirect user to feedback page
+    }
+
+    private void toPaymentInfo() {
+        //TODO: Redirect user to payment page
+    }
+
+    private void toParkingHistory() {
+        //TODO: Redirect user to parking history
+    }
+
+    private void toAccountSettings() {
+        //TODO: Redirect user to account settings page
     }
 }
